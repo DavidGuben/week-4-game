c5f67cbc38c55d49cb63e6649c8bf54306bd7020
DavidGuben <davidjg6@gmail.com>
2016-05-28 13:19:00 -0400

    added random number generator into js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 //The player will be shown a random number at the start of the game.
 //The random number should be between 19 - 120.
-x = Math.floor((Math.random() * 120) + 19);
-$("#button1").innerHTML = x;
+var x = Math.floor((Math.random() * 120) + 19);
+window.onload = function() {
+	document.getElementById('p1').innerHTML = x;
+}
